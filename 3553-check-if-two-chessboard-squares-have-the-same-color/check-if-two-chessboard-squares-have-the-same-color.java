1b865ca920c0c66c5bf086c29f28e212d82e364e
Anushka Surya <152779968+anushka1810@users.noreply.github.com>
2025-02-20 19:05:39 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (64.10%) - LeetSync

--- 3553-check-if-two-chessboard-squares-have-the-same-color/check-if-two-chessboard-squares-have-the-same-color.java
+++ 3553-check-if-two-chessboard-squares-have-the-same-color/check-if-two-chessboard-squares-have-the-same-color.java
@@ -1,7 +1,6 @@
 class Solution {
     public String isvalid(char a,char b){
         int sum=a+b;
-        System.out.println(sum);
         if((a>96 && a<105) && (b>48 && b<58 )){
             if(sum%2==0){
                 return "black";
@@ -17,8 +16,6 @@ class Solution {
         if(arr1.length==2 && arr2.length==2){
             String s1=isvalid(arr1[0],arr1[1]);
             String s2=isvalid(arr2[0],arr2[1]);
-            System.out.println(s1);
-            System.out.println(s2);
             if(s1.equals(s2)){
                 return true;
             }
