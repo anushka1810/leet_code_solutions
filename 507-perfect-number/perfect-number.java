929196995cec34f06c63baff4e36cffb21fc60ef
Anushka Surya <152779968+anushka1810@users.noreply.github.com>
2025-03-23 23:41:40 +0530

    Time: 2142 ms (5.11%) | Memory: 40.2 MB (95.00%) - LeetSync

--- 507-perfect-number/perfect-number.java
+++ 507-perfect-number/perfect-number.java
@@ -2,6 +2,9 @@ class Solution {
     public boolean checkPerfectNumber(int num) {
         int sum_div=0;
         int i=1;
+        if(num<0){
+            return false;
+        }
         while(i<num){
             if(num%i==0){
                 sum_div=sum_div+i;
