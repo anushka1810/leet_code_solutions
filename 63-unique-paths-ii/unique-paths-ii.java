1ea2e6206085502d031632e13cb05b9cd93ce818
Anushka Surya <anushkasurya803@gmail.com>
2026-02-20 11:49:53 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (25.16%) - LeetSync

---
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int r=obstacleGrid.length;
+        int c=obstacleGrid[0].length;
+        int[][] dp=new int[r][c];
+        if(obstacleGrid[0][0]==1 || obstacleGrid[r-1][c-1]==1) return 0;
+        // for(int i=0;i<r;i++){
+        //     for(int j=0;j<c;j++){
+        //         if(i==0||j==0){
+        //             if(obstacleGrid[i][j]!=1) dp[i][j]=1;
+        //         }
+        //     }
+        // }
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                if(i==0 && j==0) dp[i][j]=1;
+                else if(i==0){
+                    if(obstacleGrid[i][j]==1) dp[i][j]=0;
+                    else dp[i][j]=dp[i][j-1];
+                }
+                else if(j==0){
+                    if(obstacleGrid[i][j]==1) dp[i][j]=0;
+                    else dp[i][j]=dp[i-1][j];
+                }
+                else if(obstacleGrid[i][j]==1){
+                    dp[i][j]=0;
+                }else{
+                    dp[i][j]=dp[i-1][j]+dp[i][j-1];
+                }
+            }
+        }
+        return dp[r-1][c-1];
+
+    }
+}
