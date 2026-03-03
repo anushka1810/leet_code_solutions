a4115e81d40085c0525ebe74dd0b9268475a5e7a
Anushka Surya <anushkasurya803@gmail.com>
2026-03-03 13:44:18 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (92.23%) - LeetSync

--- 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.java
+++ 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.java
@@ -14,25 +14,44 @@
  * }
  */
 class Solution {
-    int ans=0;
-    public void solve(TreeNode root,StringBuilder val){
+    // int ans=0;
+    // public void solve(TreeNode root,StringBuilder val){
         
-        val.append(root.val);
-        int size=val.length();
+    //     val.append(root.val);
+    //     int size=val.length();
+    //     if(root.left==null && root.right==null){
+    //         int a=Integer.valueOf(val.toString());
+    //         ans+=(a);
+    //     }else{
+    //         if(root.left!=null) solve(root.left,val);
+    //         if(root.right!=null) solve(root.right,val);
+    //     }
+    //     val.deleteCharAt(size-1);
+
+        
+    // }
+    // public int sumNumbers(TreeNode root) {
+    //      if(root==null) return 0;
+    //      solve(root,new StringBuilder());
+    //      return ans;
+    // }
+
+
+    int ans=0;
+    public void solve(TreeNode root,int val){
+        int prev=val;
+        val=(val*10)+root.val;
         if(root.left==null && root.right==null){
-            int a=Integer.valueOf(val.toString());
-            ans+=(a);
+            ans+=val;
         }else{
             if(root.left!=null) solve(root.left,val);
             if(root.right!=null) solve(root.right,val);
         }
-        val.deleteCharAt(size-1);
-
-        
+        val=prev;  
     }
     public int sumNumbers(TreeNode root) {
          if(root==null) return 0;
-         solve(root,new StringBuilder());
+         solve(root,0);
          return ans;
     }
 }
