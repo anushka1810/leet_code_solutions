f54984ac3e59f37200dcdec385b1b387278bb198
Anushka Surya <anushkasurya803@gmail.com>
2026-03-08 00:11:10 +0530

    Time: 0 ms (100.00%) | Memory: 46.6 MB (55.99%) - LeetSync

--- 623-add-one-row-to-tree/add-one-row-to-tree.java
+++ 623-add-one-row-to-tree/add-one-row-to-tree.java
@@ -14,6 +14,62 @@
  * }
  */
 class Solution {
+    // public TreeNode addOneRow(TreeNode root, int val, int depth) {
+    //     if(depth==1){
+    //         TreeNode new_node=new TreeNode(val);
+    //         new_node.left=root;
+    //         return new_node;
+    //     }
+
+    //     Queue<TreeNode> q=new LinkedList<>();
+    //     q.add(root);
+    //     int d=1;
+    //     while(!q.isEmpty()){
+    //         int size=q.size();
+    //         if(d==depth-1){
+    //             for(int i=0;i<size;i++){
+    //                 TreeNode node=q.poll();
+    //                 TreeNode leftchild=node.left;
+    //                 TreeNode rightchild=node.right;
+    //                 TreeNode new_nodel=new TreeNode(val);
+    //                 TreeNode new_noder=new TreeNode(val);
+    //                 node.left=new_nodel;
+    //                 node.right=new_noder;
+    //                 new_nodel.left=leftchild;
+    //                 new_noder.right=rightchild;
+    //             }
+    //         }else{
+    //             for(int i=0;i<size;i++){
+    //                 TreeNode node=q.poll();
+    //                 if(node.left!=null) q.add(node.left);
+    //                 if(node.right!=null) q.add(node.right);
+    //             }
+    //         }
+    //         d++;
+    //     }
+    //     return root;
+    // }
+
+    public void dfs(TreeNode root, int val, int depth,int height){
+        if(root==null) return;
+
+        if(height==depth-1){
+            TreeNode leftchild=root.left;
+            TreeNode rightchild=root.right;
+            TreeNode new_nodel=new TreeNode(val);
+            TreeNode new_noder=new TreeNode(val);
+            root.left=new_nodel;
+            root.right=new_noder;
+            new_nodel.left=leftchild;
+            new_noder.right=rightchild;
+            return;
+        }
+
+        dfs(root.left,val,depth,height+1);
+        dfs(root.right,val,depth,height+1);
+    
+    }
+
     public TreeNode addOneRow(TreeNode root, int val, int depth) {
         if(depth==1){
             TreeNode new_node=new TreeNode(val);
@@ -21,32 +77,7 @@ class Solution {
             return new_node;
         }
 
-        Queue<TreeNode> q=new LinkedList<>();
-        q.add(root);
-        int d=1;
-        while(!q.isEmpty()){
-            int size=q.size();
-            if(d==depth-1){
-                for(int i=0;i<size;i++){
-                    TreeNode node=q.poll();
-                    TreeNode leftchild=node.left;
-                    TreeNode rightchild=node.right;
-                    TreeNode new_nodel=new TreeNode(val);
-                    TreeNode new_noder=new TreeNode(val);
-                    node.left=new_nodel;
-                    node.right=new_noder;
-                    new_nodel.left=leftchild;
-                    new_noder.right=rightchild;
-                }
-            }else{
-                for(int i=0;i<size;i++){
-                    TreeNode node=q.poll();
-                    if(node.left!=null) q.add(node.left);
-                    if(node.right!=null) q.add(node.right);
-                }
-            }
-            d++;
-        }
+        dfs(root,val,depth,1);
         return root;
     }
 }
