ce0bd0cc32733ac52621f09cc43e4aa44269dad3
Anushka Surya <anushkasurya803@gmail.com>
2026-03-07 20:49:12 +0530

    Time: 2 ms (100.00%) | Memory: 47.6 MB (47.63%) - LeetSync

--- 508-most-frequent-subtree-sum/most-frequent-subtree-sum.java
+++ 508-most-frequent-subtree-sum/most-frequent-subtree-sum.java
@@ -14,32 +14,43 @@
  * }
  */
 class Solution {
-    int max=Integer.MIN_VALUE;
-    int count=0;
-    public int solve(TreeNode root,HashMap<Integer,Integer> h){
-        if(root==null){
+    List<Integer> ans = new ArrayList<>();
+    int max = Integer.MIN_VALUE;
+    int count = 0;
+
+    public int solve(TreeNode root, HashMap<Integer, Integer> h) {
+        if (root == null) {
             return 0;
         }
-        int sum=solve(root.left,h)+ solve(root.right,h)+root.val;
-        h.put(sum,h.getOrDefault(sum,0)+1);
-        if(h.get(sum)>max){
-            max=h.get(sum);
-            count=1;
-        }else if(h.get(sum)==max){
-            count++;
+        int sum = solve(root.left, h) + solve(root.right, h) + root.val;
+        h.put(sum, h.getOrDefault(sum, 0) + 1);
+        if (h.get(sum) > max) {
+            ans.clear();
+            ans.add(sum);
+            max = h.get(sum);
+            count = 1;
+        } else if (h.get(sum) == max) {
+            ans.add(sum);
         }
         return sum;
     }
+
     public int[] findFrequentTreeSum(TreeNode root) {
-        HashMap<Integer,Integer> h=new HashMap<>();
-        solve(root,h);
-        int ans[]=new int[count];
-        int k=0;
-        for(int num:h.keySet()){
-            if(h.get(num)==max){
-                ans[k++]=num;
-            }
+        HashMap<Integer, Integer> h = new HashMap<>();
+        solve(root, h);
+        // int ans[]=new int[count];
+        // int k=0;
+        // for(int num:h.keySet()){
+        //     if(h.get(num)==max){
+        //         ans[k++]=num;
+        //     }
+        // }
+        // return ans;
+        int[] res = new int[ans.size()];
+
+        for (int i = 0; i < ans.size(); i++) {
+            res[i] = ans.get(i);
         }
-        return ans;
+        return res;
     }
 }
