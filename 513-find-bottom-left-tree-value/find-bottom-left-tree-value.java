b8574de5429d747947afaf73906117472ab46aca
Anushka Surya <anushkasurya803@gmail.com>
2026-03-07 21:15:33 +0530

    Time: 0 ms (100.00%) | Memory: 46.5 MB (52.40%) - LeetSync

--- 513-find-bottom-left-tree-value/find-bottom-left-tree-value.java
+++ 513-find-bottom-left-tree-value/find-bottom-left-tree-value.java
@@ -14,23 +14,42 @@
  * }
  */
 class Solution {
-    
+    // public int solve(TreeNode root){
+    //     Queue<TreeNode> q=new LinkedList<>();
+    //     q.add(root);
+    //     int ans=root.val;
+
+    //     while(!q.isEmpty()){
+    //         int size=q.size();
+
+    //         for(int i=0;i<size;i++){
+    //             TreeNode node=q.poll();
+    //             if(i==0) ans=node.val;
+    //             if(node.left!=null) q.add(node.left);
+    //             if(node.right!=null) q.add(node.right);
+    //         }   
+    //     }
+    //     return ans;
+    // }
+    // public int findBottomLeftValue(TreeNode root) {
+    //     if(root==null) return 0;
+    //     return solve(root);
+    // }
+    int ans=-1;
+    int maxheight=-1;
+    public void dfs(TreeNode root,int height){
+        if(root==null) return;
+        if(height>maxheight){
+            maxheight=height;
+            ans=root.val;
+        }
+        dfs(root.left,height+1);
+        dfs(root.right,height+1);
+    }
     public int findBottomLeftValue(TreeNode root) {
         if(root==null) return 0;
-        Queue<TreeNode> q=new LinkedList<>();
-        q.add(root);
-        int ans=root.val;
-
-        while(!q.isEmpty()){
-            int size=q.size();
-
-            for(int i=0;i<size;i++){
-                TreeNode node=q.poll();
-                if(i==0) ans=node.val;
-                if(node.left!=null) q.add(node.left);
-                if(node.right!=null) q.add(node.right);
-            }   
-        }
+        if(root.left==null && root.right==null) return root.val;
+        dfs(root,0);
         return ans;
     }
 }
