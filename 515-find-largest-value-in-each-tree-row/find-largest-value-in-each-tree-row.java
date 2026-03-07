3c3ffec2181a0c092630d25ce9ef397db9198613
Anushka Surya <anushkasurya803@gmail.com>
2026-03-07 23:09:30 +0530

    Time: 1 ms (98.54%) | Memory: 46.6 MB (89.80%) - LeetSync

--- 515-find-largest-value-in-each-tree-row/find-largest-value-in-each-tree-row.java
+++ 515-find-largest-value-in-each-tree-row/find-largest-value-in-each-tree-row.java
@@ -15,25 +15,45 @@
  */
 class Solution {
     
+    // public List<Integer> largestValues(TreeNode root) {
+    //     // best option is bfs
+    //     List<Integer> ans=new ArrayList<>();
+    //     if(root==null) return ans;
+    //     Queue<TreeNode> q=new LinkedList<>();
+    //     q.add(root);
+
+    //     while(!q.isEmpty()){
+    //         int size=q.size();
+
+    //         int max=q.peek().val;
+    //         for(int i=0;i<size;i++){
+    //             TreeNode node=q.poll();
+    //             if(node.val>max) max=node.val;
+    //             if(node.left!=null) q.add(node.left);
+    //             if(node.right!=null) q.add(node.right);
+    //         }   
+    //         ans.add(max);
+    //     }
+    //     return ans;
+    // }
+
+    public void dfs(TreeNode root,int height,List<Integer> ans){
+        if(root==null) return;
+
+        if(ans.size()<=height){
+            ans.add(root.val);
+        }else if( ans.get(height)<root.val){
+            ans.set(height,root.val);
+        }
+        dfs(root.left,height+1,ans);
+        dfs(root.right,height+1,ans);
+
+    }
+
     public List<Integer> largestValues(TreeNode root) {
-        // best option is bfs
         List<Integer> ans=new ArrayList<>();
         if(root==null) return ans;
-        Queue<TreeNode> q=new LinkedList<>();
-        q.add(root);
-
-        while(!q.isEmpty()){
-            int size=q.size();
-
-            int max=q.peek().val;
-            for(int i=0;i<size;i++){
-                TreeNode node=q.poll();
-                if(node.val>max) max=node.val;
-                if(node.left!=null) q.add(node.left);
-                if(node.right!=null) q.add(node.right);
-            }   
-            ans.add(max);
-        }
+        dfs(root,0,ans);
         return ans;
     }
 }
