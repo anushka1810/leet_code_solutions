4c6e15ebbfc006b1c1d76f2e495e0b529edfab53
Anushka Surya <anushkasurya803@gmail.com>
2026-03-12 20:36:06 +0530

    Time: 6 ms (97.13%) | Memory: 47.6 MB (72.17%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.java
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.java
@@ -9,18 +9,37 @@
  */
 
 class Solution {
-    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(root==null || root==p || root==q) return root;
+    // public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+    //     if(root==null || root==p || root==q) return root;
 
-        TreeNode left=lowestCommonAncestor(root.left,p,q);
-        TreeNode right=lowestCommonAncestor(root.right,p,q);
+    //     TreeNode left=lowestCommonAncestor(root.left,p,q);
+    //     TreeNode right=lowestCommonAncestor(root.right,p,q);
 
-        if(left==null){
-            return right;
-        }else if(right==null){
-            return left;
-        }else{
-            return root;
+    //     if(left==null){
+    //         return right;
+    //     }else if(right==null){
+    //         return left;
+    //     }else{
+    //         return root;
+    //     }
+    // }
+
+
+     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q){
+        if(root==null) return null;
+
+
+        while(root!=null){
+            if(root.val==p.val || root.val==q.val){
+                return root;
+            }else if(root.val>p.val && root.val>q.val){
+                root=root.left;
+            }else if(root.val<p.val && root.val<q.val){
+                root=root.right;
+            }else{
+                return root;
+            }
         }
-    }
+        return null;
+     }
 }
