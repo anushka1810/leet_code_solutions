f91ed191ad46ae929042f10bb561cd78174981a1
Anushka Surya <anushkasurya803@gmail.com>
2026-04-06 01:50:24 +0530

    Time: 1 ms (82.81%) | Memory: 42.6 MB (80.14%) - LeetSync

---
+++ 290-word-pattern/word-pattern.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        String word[]=s.split(" ");
+        if(word.length!=pattern.length()) return false;
+        HashMap<Character,String> h=new HashMap<>();
+        HashMap<String,Character> h2=new HashMap<>();
+        for(int i=0;i<word.length;i++){
+            char ch=pattern.charAt(i);
+            String w=word[i];
+
+            if(h.containsKey(ch)){
+                if(!h.get(ch).equals(w)) return false;
+            }else if(h2.containsKey(w)){
+                if(!h2.get(w).equals(ch)) return false;
+            }else{
+                h.put(ch,w);
+                h2.put(w,ch);
+            }  
+        }
+        return true;
+    }
+}
