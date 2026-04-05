1269bef8d201bdba2a51a7b2dea8a731f4678bdf
Anushka Surya <anushkasurya803@gmail.com>
2026-04-06 01:53:30 +0530

    Time: 1 ms (82.81%) | Memory: 42.4 MB (97.58%) - LeetSync

--- 290-word-pattern/word-pattern.java
+++ 290-word-pattern/word-pattern.java
@@ -3,39 +3,39 @@ class Solution {
     //     String word[]=s.split(" ");
     //     if(word.length!=pattern.length()) return false;
     //     HashMap<Character,String> h=new HashMap<>();
-    //     HashMap<String,Character> h2=new HashMap<>();
     //     for(int i=0;i<word.length;i++){
     //         char ch=pattern.charAt(i);
     //         String w=word[i];
 
     //         if(h.containsKey(ch)){
     //             if(!h.get(ch).equals(w)) return false;
-    //         }else if(h2.containsKey(w)){
-    //             if(!h2.get(w).equals(ch)) return false;
+    //         }else if(h.containsValue(w)){
+    //             return false;
     //         }else{
     //             h.put(ch,w);
-    //             h2.put(w,ch);
     //         }  
     //     }
     //     return true;
     // }
-
     public boolean wordPattern(String pattern, String s) {
         String word[]=s.split(" ");
         if(word.length!=pattern.length()) return false;
         HashMap<Character,String> h=new HashMap<>();
+        HashMap<String,Character> h2=new HashMap<>();
         for(int i=0;i<word.length;i++){
             char ch=pattern.charAt(i);
             String w=word[i];
 
             if(h.containsKey(ch)){
                 if(!h.get(ch).equals(w)) return false;
-            }else if(h.containsValue(w)){
-                return false;
+            }else if(h2.containsKey(w)){
+                if(!h2.get(w).equals(ch)) return false;
             }else{
                 h.put(ch,w);
+                h2.put(w,ch);
             }  
         }
         return true;
     }
+
 }
