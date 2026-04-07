fd76818a94538b3b99b2deed421eb2d5b0ab3912
Anushka Surya <anushkasurya803@gmail.com>
2026-04-07 22:48:42 +0530

    Time: 0 ms (100.00%) | Memory: 44.9 MB (61.56%) - LeetSync

---
+++ 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.java
@@ -0,0 +1,86 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    // public ListNode deleteDuplicates(ListNode head) {
+    //     if(head==null || head.next==null) return head;
+    //     ListNode temp=head.next;
+    //     ListNode prev=head;
+    //     ListNode dummy=new ListNode(-101,prev);
+    //     ListNode prev2prev=dummy;
+
+    //     while(temp!=null){
+    //         if(prev.val!=temp.val){
+    //             prev.next=temp;
+    //             prev2prev=prev;
+    //             prev=temp;
+    //         }else{
+    //             while(temp!=null && prev.val==temp.val) temp=temp.next;
+    //             prev2prev.next=temp;
+    //             prev=temp;
+    //         }
+    //         if(temp==null) break;
+    //         temp=temp.next;
+    //     }
+    //     return dummy.next;
+    // }
+
+
+    
+    // public ListNode deleteDuplicates(ListNode head) {
+    //     if(head==null || head.next==null) return head;
+    //     ListNode temp=head;
+    //     ListNode dummy=new ListNode();
+    //     ListNode prev=dummy;
+
+    //     while(temp!=null){
+    //         if(temp.next!=null && temp.val==temp.next.val){
+    //             int val=temp.val;
+    //             while(temp!=null && temp.val==val){
+    //                 temp=temp.next;
+    //             }
+    //             prev.next=temp;
+    //         }else{
+    //             prev.next=temp;
+    //             prev=temp;
+    //             temp=temp.next;
+
+    //         }
+    //     }
+    //     return dummy.next;
+    // }
+
+
+    public ListNode deleteDuplicates(ListNode head) {
+        if(head==null||head.next==null) return head;
+
+        ListNode dummy=new ListNode(-1);
+        ListNode ansList=dummy;
+        ListNode prev=head;
+        ListNode temp=head;
+
+        while(temp!=null){
+            while(temp!=null && temp.val==prev.val){
+                temp=temp.next;
+            }
+            if(prev.next==temp){
+                ansList.next=prev;
+                ansList=ansList.next;
+            }
+
+            prev=temp;
+
+        }
+        ansList.next=null;
+
+        return dummy.next;
+    }
+
+}
