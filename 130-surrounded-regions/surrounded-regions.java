164818fd27eb18cb8fa30c753ffb781ed6f87ecf
Anushka Surya <anushkasurya803@gmail.com>
2026-04-10 14:44:02 +0530

    Time: 2 ms (78.84%) | Memory: 47.6 MB (78.55%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.java
+++ 130-surrounded-regions/surrounded-regions.java
@@ -1,6 +1,5 @@
 class Solution {
-    public void dfs(int i,int j,char[][] board,boolean[][] visited,int m,int n,int[][] direction){
-        visited[i][j]=true;
+    public void dfs(int i,int j,char[][] board,int m,int n,int[][] direction){
 
         board[i][j]='#';
 
@@ -8,8 +7,8 @@ class Solution {
             int ni=i+dir[0];
             int nj=j+dir[1];
 
-            if(ni>=0 && nj>=0 && ni<m && nj<n && !visited[ni][nj] && board[ni][nj]=='O'){
-                dfs(ni,nj,board,visited,m,n,direction);
+            if(ni>=0 && nj>=0 && ni<m && nj<n && board[ni][nj]=='O'){
+                dfs(ni,nj,board,m,n,direction);
             }
         }
     }
@@ -17,14 +16,12 @@ class Solution {
         int m=board.length;
         int n=board[0].length;
 
-        Queue<Integer> q=new LinkedList<>();
-        boolean visited[][]=new boolean[m][n];
         int direction[][]={{1,0},{-1,0},{0,1},{0,-1}};
 
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if((i==0 || j==0 || i==m-1 || j==n-1)&& !visited[i][j] && board[i][j]=='O'){
-                    dfs(i,j,board,visited,m,n,direction);
+                if((i==0 || j==0 || i==m-1 || j==n-1) && board[i][j]=='O'){
+                    dfs(i,j,board,m,n,direction);
                 }
             }
         }
