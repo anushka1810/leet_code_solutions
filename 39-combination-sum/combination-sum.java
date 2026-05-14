7d0d23034bc674f639fc27ef7cb64adb9e2e7b23
Anushka Surya <anushkasurya803@gmail.com>
2026-05-14 19:41:58 +0530

    Time: 2 ms (94.88%) | Memory: 46.1 MB (10.11%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,30 +1,11 @@
 class Solution {
-    // public void solve(int[] candidates, int target,List<List<Integer>> ans,int idx,int sum,List<Integer> path){
-    //     int size=path.size();
-    //     if(idx==candidates.length) return;
-    //     sum+=candidates[idx];
-    //     path.add(candidates[idx]);
-    //     if(sum==target){
-    //         ans.add(path);
-    //     }else if(sum<target){
-    //         solve(candidates,target,ans,idx,sum,path);
-    //     }
-    //     path.remove(size);
-    // }
-    // public List<List<Integer>> combinationSum(int[] candidates, int target) {
-    //     List<List<Integer>> ans=new ArrayList<>();
-    //     List<Integer> path=new ArrayList<>();
-
-    //     solve(candidates,target,ans,0,0,path);
-    //     return ans;
-    // }
-
     public void solve(int[] candidates, int target,List<List<Integer>> ans,int idx,List<Integer> path){
-        if(idx==candidates.length || target<0) return;
         if(target==0){
             ans.add(new ArrayList<>(path));
             return ;
-        }else if(candidates[idx]<=target){
+        }
+        if(idx==candidates.length || target<0) return;
+        if(candidates[idx]<=target){
             path.add(candidates[idx]);
             solve(candidates,target-candidates[idx],ans,idx,path);
             path.remove(path.size()-1);
