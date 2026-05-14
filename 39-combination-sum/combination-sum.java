3d6704ea11323cc13cc3c200eaf03e5c985a36d8
Anushka Surya <anushkasurya803@gmail.com>
2026-05-14 19:44:17 +0530

    Time: 2 ms (94.88%) | Memory: 45.9 MB (32.61%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,22 +1,22 @@
 class Solution {
-    public void solve(int[] candidates, int target,List<List<Integer>> ans,int idx,List<Integer> path){
-        if(target==0){
+    public void solve(int[] candidates,int sum, int target,List<List<Integer>> ans,int idx,List<Integer> path){
+        if(sum==target){
             ans.add(new ArrayList<>(path));
             return ;
         }
-        if(idx==candidates.length || target<0) return;
-        if(candidates[idx]<=target){
+        if(idx==candidates.length || sum>target) return;
+        if(candidates[idx]+sum<=target){
             path.add(candidates[idx]);
-            solve(candidates,target-candidates[idx],ans,idx,path);
+            solve(candidates,sum+candidates[idx],target,ans,idx,path);
             path.remove(path.size()-1);
         }
-        solve(candidates,target,ans,idx+1,path);
+        solve(candidates,sum,target,ans,idx+1,path);
     }
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> ans=new ArrayList<>();
         List<Integer> path=new ArrayList<>();
 
-        solve(candidates,target,ans,0,path);
+        solve(candidates,0,target,ans,0,path);
         return ans;
     }
 }
