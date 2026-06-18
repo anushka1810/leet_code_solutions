73cb669d28b5c91471684df5358586a84cbc2966
Anushka Surya <anushkasurya803@gmail.com>
2026-06-18 20:07:14 +0530

    Time: 2 ms (99.72%) | Memory: 44.4 MB (99.08%) - LeetSync

--- 90-subsets-ii/subsets-ii.java
+++ 90-subsets-ii/subsets-ii.java
@@ -1,24 +1,25 @@
 class Solution {
-    public void helper(int nums[],int idx, List<List<Integer>> ans,List<Integer> path){
+    public void solve(int[] nums,List<List<Integer>> ans,List<Integer> temp,int idx){
         if(idx==nums.length){
-            ans.add(new ArrayList<>(path));
+            ans.add(new ArrayList<>(temp));
             return;
         }
 
-        //not pick
-        path.add(nums[idx]);
-        helper(nums,idx+1,ans,path);
-        path.remove(path.size()-1);
+        //pick
+        temp.add(nums[idx]);
+        solve(nums,ans,temp,idx+1);
+        temp.remove(temp.size()-1);
 
-        while(idx+1<nums.length && nums[idx+1]==nums[idx]) idx++;
-        helper(nums,idx+1,ans,path);
+        //not pick
+        //all occurence should be not pick 
+        while(idx<nums.length-1 && nums[idx]==nums[idx+1]) idx++;
+        solve(nums,ans,temp,idx+1);
     }
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-        Arrays.sort(nums);
         List<List<Integer>> ans=new ArrayList<>();
-        List<Integer> path=new ArrayList<>();
-
-        helper(nums,0,ans,path);
+        List<Integer> temp=new ArrayList<>();
+        Arrays.sort(nums);
+        solve(nums,ans,temp,0);
         return ans;
     }
 }
