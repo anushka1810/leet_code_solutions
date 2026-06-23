077cdae21a6bd08faadb91958906f374ba859f41
Anushka Surya <anushkasurya803@gmail.com>
2026-06-23 11:32:41 +0530

    Time: 2 ms (23.56%) | Memory: 43.3 MB (15.56%) - LeetSync

---
+++ 1923-sentence-similarity-iii/sentence-similarity-iii.java
@@ -0,0 +1,49 @@
+class Solution {
+    public boolean areSentencesSimilar(String sentence1, String sentence2) {
+        // String[] s1 = sentence1.split(" ");
+        // String[] s2 = sentence2.split(" ");
+        // int n1=s1.length;
+        // int n2=s2.length;
+        // if(n1==n2) return sentence1.equals(sentence2); 
+
+        // if(n1<n2) return  areSentencesSimilar(sentence2,sentence1);
+
+        // int i=0,j=sentence2.length()-1;
+        // int i2=0,j2=sentence1.length()-1;
+
+        // while(i<sentence2.length() && sentence1.charAt(i2)==sentence2.charAt(i)){
+        //     i++;
+        //     i2++;
+        // }
+        // while(j>=0 && sentence1.charAt(j2)==sentence2.charAt(j)){
+        //     j--;
+        //     j2--;
+        // }
+
+        // return (j<i)?true:false;
+
+
+        String[] s1 = sentence1.split(" ");
+        String[] s2 = sentence2.split(" ");
+        int n1=s1.length;
+        int n2=s2.length;
+
+        if(n1<n2) return  areSentencesSimilar(sentence2,sentence1);
+
+        int i1=0,j1=n1-1;
+        int i2=0,j2=n2-1;
+
+        while(i2<n2 && s1[i1].equals(s2[i2])){
+            i1++;
+            i2++;
+        }
+        while(j2>=0 && s1[j1].equals(s2[j2])){
+            j1--;
+            j2--;
+        }
+
+        return (j2<i2);
+
+        
+    }
+}
