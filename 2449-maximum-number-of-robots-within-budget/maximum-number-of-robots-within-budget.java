e4548d9b8809d58d796f03b8164879e3e154c599
Anushka Surya <anushkasurya803@gmail.com>
2026-06-24 12:40:01 +0530

    Time: 99 ms (14.83%) | Memory: 72.6 MB (27.76%) - LeetSync

--- 2449-maximum-number-of-robots-within-budget/maximum-number-of-robots-within-budget.java
+++ 2449-maximum-number-of-robots-within-budget/maximum-number-of-robots-within-budget.java
@@ -1,55 +1,80 @@
 class Solution {
-    public int budgetReqToTakeMidSub(long budget,int[] arrC, int[] arrR){
+    // public int budgetReqToTakeMidSub(long budget,int[] arrC, int[] arrR){
+    //     int i=0,j=0;
+    //     // int max=arr[0];
+    //     ArrayDeque<Integer> max=new ArrayDeque<>();
+    //     long sum=0;
+    //     long minBudget=Long.MAX_VALUE;
+    //     // int k=mid;
+    //     int ans=0;
+
+    //     while(j<arrR.length){
+    //         while(!max.isEmpty() && max.getLast()<arrC[j]) max.pollLast();
+    //         max.add(arrC[j]);
+    //         sum+=arrR[j];
+
+    //         while(i<=j && (max.peek()+(long)(j-i+1)*sum)>budget){
+    //             sum-=arrR[i];
+    //             if(arrC[i]==max.peek()) max.pollFirst();
+    //             i++;
+    //         }
+
+    //         if( i<=j && (max.peek()+(long)(j-i+1)*sum)<=budget) ans=Math.max(ans,j-i+1);
+    //         j++;
+    //     }
+    //     return ans;
+    // }
+    // public int maximumRobots(int[] chargeTimes, int[] runningCosts, long budget) {
+
+    //     return budgetReqToTakeMidSub(budget,chargeTimes,runningCosts);
+        
+    // }
+    public boolean check(int[] chargeTimes, int[] runningCosts, long budget,int k){
+
+        if(k==0) return true;
         int i=0,j=0;
-        // int max=arr[0];
-        ArrayDeque<Integer> max=new ArrayDeque<>();
         long sum=0;
-        long minBudget=Long.MAX_VALUE;
-        // int k=mid;
-        int ans=0;
+        ArrayDeque<Integer> dq=new ArrayDeque<>();
 
-        while(j<arrR.length){
-            while(!max.isEmpty() && max.getLast()<arrC[j]) max.pollLast();
-            max.add(arrC[j]);
-            sum+=arrR[j];
+        while(j<chargeTimes.length){
+            sum+=runningCosts[j];
+            while(!dq.isEmpty() && dq.peekLast()<chargeTimes[j]){
+                dq.pollLast();
+            }
+            dq.addLast(chargeTimes[j]);
 
-            // long bud=max.peek()+(long)k*sum;
-            while(i<=j && (max.peek()+(long)(j-i+1)*sum)>budget){
-                sum-=arrR[i];
-                if(arrC[i]==max.peek()) max.pollFirst();
+            if(j-i+1==k){
+                long budReq=(long)dq.peek()+(long)(k*sum);
+                if(budReq<=budget) return true;
+
+                if(dq.peek()==chargeTimes[i]){
+                    dq.pollFirst();
+                }
+                sum-=runningCosts[i];
                 i++;
             }
-
-            if( i<=j && (max.peek()+(long)(j-i+1)*sum)<=budget) ans=Math.max(ans,j-i+1);
             j++;
-
-            // if(j-i+1<k) j++;
-            // else if(j-i+1==k){
-            //     if(bud<minBudget) minBudget=bud;
-
-            // }
         }
-        return ans;
+        return false;
     }
     public int maximumRobots(int[] chargeTimes, int[] runningCosts, long budget) {
-        // int n=chargeTimes.length;
 
-        // int low=1,high=n;
-        // int ans=0;
+        int low=0,high=chargeTimes.length;
+        int ans=0;
 
-        // while(low<=high){
-        //     int mid=low+(high-low)/2;
+        while(low<=high){
+            int mid=low+(high-low)/2;
 
-        //     long b=budgetReqToTakeMidSub(mid,chargeTimes,runningCosts);
-        //     if(b>budget){
-        //         high=mid-1;
-        //     }else{
-        //         ans=mid;
-        //         low=mid+1;
-        //     }
-        // }
+            if(check(chargeTimes,runningCosts,budget,mid)){
+                ans=mid;
+                low=mid+1;
+            }else{
+                high=mid-1;
+            }
+        }
+        return ans;
 
-        return budgetReqToTakeMidSub(budget,chargeTimes,runningCosts);
+
         
     }
 }
