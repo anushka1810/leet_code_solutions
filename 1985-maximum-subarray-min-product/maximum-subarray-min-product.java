abffaff46507d7594769c50006a56bdbf91f14ca
Anushka Surya <anushkasurya803@gmail.com>
2026-07-01 14:21:29 +0530

    Time: 150 ms (5.64%) | Memory: 89.1 MB (60.53%) - LeetSync

--- 1985-maximum-subarray-min-product/maximum-subarray-min-product.java
+++ 1985-maximum-subarray-min-product/maximum-subarray-min-product.java
@@ -1,16 +1,9 @@
 class Solution {
-    public int maxSumMinProduct(int[] nums) {
-
-
+    // Easy to understand iske baad submiited answer is thora optimise 
+    public int[] nse(int[] nums){
         int n=nums.length;
         int nse[]=new int[n];
         Stack<Integer> st=new Stack<>();
-
-        int pse[]=new int[n];
-        Stack<Integer> st2=new Stack<>();
-
-        long[] prefixSum=new long[n];
-        long sum=0;
 
         for(int i=n-1;i>=0;i--){
             while(!st.isEmpty() && nums[st.peek()]>=nums[i]){
@@ -19,23 +12,37 @@ class Solution {
             if(st.isEmpty()) nse[i]=-1;
             else nse[i]=st.peek();
             st.push(i);
+        }
+        return nse;
+    }
+    public int[] pse(int[] nums){
+        int n=nums.length;
+        int pse[]=new int[n];
+        Stack<Integer> st=new Stack<>();
 
-            int j=n-1-i;
-            while(!st2.isEmpty() && nums[st2.peek()]>=nums[j]){
-                st2.pop();
+        for(int i=0;i<n;i++){
+            while(!st.isEmpty() && nums[st.peek()]>=nums[i]){
+                st.pop();
             }
-            if(st2.isEmpty()) pse[j]=-1;
-            else pse[j]=st2.peek();
-            st2.push(j);
-            
-            prefixSum[j]=sum+nums[j];
-            sum=prefixSum[j];
+            if(st.isEmpty()) pse[i]=-1;
+            else pse[i]=st.peek();
+            st.push(i);
         }
 
-
+        return pse;
+    }
+    public int maxSumMinProduct(int[] nums) {
         int j=0;
+        int n=nums.length;
         long ans=Long.MIN_VALUE;
         int MOD=1_000_000_007;
+        int[] pse=pse(nums);
+        int[] nse=nse(nums);
+        long[] prefixSum=new long[n];
+        prefixSum[0]=nums[0];
+        for(int i=1;i<n;i++){
+            prefixSum[i]=prefixSum[i-1]+nums[i];
+        }
     
         while(j<n){
             long l=(pse[j]==-1)?0:prefixSum[pse[j]];
