6dd5988b46915919f62d278453d3ce40f9754f32
Anushka Surya <anushkasurya803@gmail.com>
2026-07-03 00:03:53 +0530

    Time: 4 ms (45.11%) | Memory: 52 MB (73.13%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
@@ -1,20 +1,21 @@
 class Solution {
     public int longestOnes(int[] nums, int k) {
-        // maximum length sunbarray with at most k 0 
         int i=0,j=0;
-        int max=Integer.MIN_VALUE;
+        int n=nums.length;
         int zero=0;
+        int ans=0;
 
-        while(j<nums.length){
+        while(j<n){
             if(nums[j]==0) zero++;
+
             while(i<=j && zero>k){
                 if(nums[i]==0) zero--;
                 i++;
             }
 
-            max=Math.max(max,j-i+1);
+            if(zero<=k) ans=Math.max(ans,j-i+1);
             j++;
         }
-        return max;
+        return ans;
     }
 }
