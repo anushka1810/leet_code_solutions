6ada95377c738ef06122664dc060509725202cc1
Anushka Surya <anushkasurya803@gmail.com>
2026-07-14 23:28:33 +0530

    Time: 1 ms (100.00%) | Memory: 46.2 MB (61.89%) - LeetSync

---
+++ 1184-car-pooling/car-pooling.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean carPooling(int[][] trips, int capacity) {
+       int[] segment=new int[1001];
+       int max=0;
+
+       for(int[] trip:trips){
+        int number=trip[0];
+        int from=trip[1];
+        int to=trip[2];
+
+        segment[from]+=number;
+        segment[to]-=number;
+        if(to>max) max=to;
+       }
+
+       int prefix=0;
+       for(int i=0;i<1001;i++){
+        prefix+=segment[i];
+        if(prefix>capacity) return false;
+        if(i==max) break;
+
+       }
+       return true;
+    }
+}
