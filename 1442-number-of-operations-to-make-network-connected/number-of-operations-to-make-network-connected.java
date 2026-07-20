5793981e57cd19dfa2848c6d51af5e330d44b464
Anushka Surya <anushkasurya803@gmail.com>
2026-07-20 16:13:14 +0530

    Time: 7 ms (53.67%) | Memory: 65.5 MB (89.66%) - LeetSync

---
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.java
@@ -0,0 +1,81 @@
+class Solution {
+    class DSU{
+        int v;
+        int parent[];
+        int size[];
+        int rank[];
+
+        DSU(int v){
+            this.v=v;
+            this.parent=new int[v];
+            this.size=new int[v];
+            this.rank=new int[v];
+
+            for(int i=0;i<v;i++){
+                this.parent[i]=i;
+                this.size[i]=1;
+                this.rank[i]=0;
+            }
+        }
+
+
+        public int findParent(int x){
+            if(parent[x]==x){
+                return x;
+            }
+            return parent[x]=findParent(parent[x]);
+        }
+
+        public void unionByrank(int x,int y){
+            int parentX=findParent(x);
+            int parentY=findParent(y);
+
+            if(parentX==parentY) return;
+
+            if(rank[parentX]==rank[parentY]){
+                parent[parentY]=parentX;
+                rank[parentX]++;
+            }else if(rank[parentX]>rank[parentY]){
+                parent[parentY]=parentX;
+            }else{
+                parent[parentX]=parentY;
+            }
+        }
+        public void unionBySize(int x,int y){
+            int parentX=findParent(x);
+            int parentY=findParent(y);
+
+            if(parentX==parentY) return;
+            if(size[parentX]>=size[parentY]){
+                parent[parentY]=parentX;
+                size[parentX]+=size[parentY];
+            }else{
+                parent[parentX]=parentY;
+                size[parentY]+=size[parentX];
+            }
+        }
+    }
+    public int makeConnected(int n, int[][] connections) {
+
+        DSU obj=new DSU(n);
+
+        int couldremove=0;
+        for(int[] edge:connections){
+            int parentX=obj.findParent(edge[0]);
+            int parentY=obj.findParent(edge[1]);
+
+            if(parentX==parentY) couldremove++;
+            obj.unionByrank(edge[0],edge[1]);
+        }
+
+        int component=0;
+        for(int i=0;i<n;i++){
+            if(obj.findParent(i)==i) component++;
+        }
+
+        if(component>couldremove+1) return -1;
+
+        return component-1;
+        
+    }
+}
