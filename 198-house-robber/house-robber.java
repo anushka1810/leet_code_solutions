c8176df6ddf56c911bc0ac447af84c903f7ce387
Anushka Surya <anushkasurya803@gmail.com>
2026-07-29 11:15:55 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (5.50%) - LeetSync

---
+++ 198-house-robber/house-robber.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int helper(int [] nums,int idx,int[] dp){
+        if(idx>=nums.length){
+            return 0;
+        }
+        if(dp[idx]!=-1) return dp[idx];
+        int take=helper(nums,idx+2,dp)+nums[idx];
+        int notake=helper(nums,idx+1,dp);
+
+        return dp[idx]=Math.max(take,notake);
+
+    }
+    public int rob(int[] nums) {
+
+        int dp[]=new int[nums.length];
+        Arrays.fill(dp,-1);
+        return helper(nums,0,dp);
+        
+    }
+}
