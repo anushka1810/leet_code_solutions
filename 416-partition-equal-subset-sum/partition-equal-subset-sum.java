0c1b65ad2cfb4ec87b19766b631c1379b28886fa
Anushka Surya <anushkasurya803@gmail.com>
2026-07-30 20:52:46 +0530

    Time: 73 ms (33.56%) | Memory: 75.7 MB (17.26%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,16 +1,33 @@
 class Solution {
-
-    public Boolean solve(int[] nums,int sum,int n,Boolean[][] dp){
-        if(sum==0) return true;
-        if(n==0) return false;
-
-        if(dp[n][sum]!=null) return dp[n][sum];
-
-        if(nums[n-1]<=sum){
-            return dp[n][sum]=solve(nums,sum-nums[n-1],n-1,dp)||solve(nums,sum,n-1,dp);
-        }else {
-            return dp[n][sum]=solve(nums,sum,n-1,dp);
+    public boolean isSubsetSum(int arr[], int sum) {
+        // code here
+        
+        
+        Boolean dp[][]=new Boolean[arr.length+1][sum+1];
+        
+        // return helper(arr,sum,arr.length-1,dp);
+        
+        for(int i=0;i<=arr.length;i++){
+            dp[i][0]=true;
         }
+        for(int i=1;i<=sum;i++){
+            dp[0][i]=false;
+        }
+        
+        
+        for(int i=1;i<=arr.length;i++){
+            for(int j=1;j<=sum;j++){
+                if(arr[i-1]<=j){
+                    dp[i][j]=dp[i-1][j-arr[i-1]]||dp[i-1][j];
+                }else{
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
+        }
+        
+        return dp[arr.length][sum];
+        
+        
     }
     public boolean canPartition(int[] nums) {
         int total=0;
@@ -18,12 +35,9 @@ class Solution {
             total+=num;
         }
 
-        if(total%2!=0){
-            return false;
-        }
+        if(total%2!=0) return false;
 
-        Boolean dp[][]=new Boolean[nums.length+1][(total/2)+1];
+        return isSubsetSum(nums,total/2);
 
-        return solve(nums,total/2,nums.length,dp);
     }
 }
